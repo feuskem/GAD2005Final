4a5228f6162ae29cfa6db90cf9ea46a09ee69bf0
feuskem <118988514+feuskem@users.noreply.github.com>
2024-05-14 07:30:31 +0300

    Manpower olayı

--- Assets/Clicer/Scripts/SpawnUnits.cs
+++ Assets/Clicer/Scripts/SpawnUnits.cs
@@ -1,9 +1,21 @@
 using UnityEngine;
+using System.Collections;
 
 public class SpawnUnits : MonoBehaviour
 {
     public GameObject[] units;
+      public int[] unitManpowerCosts;
     private int selectedUnitIndex = 0;
+    private Coroutine increaseManpowerCoroutine;
+
+    int manpower = 250;
+
+
+    private void Start()
+    {
+        StartCoroutine(IncreaseManpowerOverTime());
+    }
+
 
     public void SelectUnit(int index)
     {
@@ -14,12 +26,42 @@ public class SpawnUnits : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-            if (Physics.Raycast(ray, out hit))
+            if (manpower >= unitManpowerCosts[selectedUnitIndex]) 
             {
-                Instantiate(units[selectedUnitIndex], hit.point + Vector3.up * 2, Quaternion.identity);
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    Instantiate(units[selectedUnitIndex], hit.point + Vector3.up * 2, Quaternion.identity);
+                    manpower -= unitManpowerCosts[selectedUnitIndex]; 
+                    Debug.Log("Manpower remaining: " + manpower);
+                }
             }
+            else
+            {
+                Debug.Log("Not enough manpower to spawn this unit.");
+            }
+        }
+        if (manpower < 1000 && increaseManpowerCoroutine == null)
+        {
+            increaseManpowerCoroutine = StartCoroutine(IncreaseManpowerOverTime());
+        }
+    }
+
+     private IEnumerator IncreaseManpowerOverTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(3); 
+
+            if (manpower >= 1000)
+            {
+                increaseManpowerCoroutine = null;
+                yield break;
+            }
+
+            manpower += 10; 
+            Debug.Log("Manpower increased: " + manpower);
         }
     }
 }
